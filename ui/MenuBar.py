129adc16bf11850090fc13feeb00e38f389dedae
zekeWong <kivensolo@gmail.com>
2023-12-28 12:15:06 +0800

    Title(feat): 菜单栏实现动态配置布局，目前只支持一级菜单嵌套，不支持多级。 Cause：菜单UI结构重构。 Solution：N/A。 Effect: N/A Reviewer: N/A 附加说明：N/A

--- ui/MenuBar.py
+++ ui/MenuBar.py
@@ -1,17 +1,25 @@
 from PyQt5 import QtCore
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction, qApp
+from PyQt5.QtWidgets import QAction, qApp, QMenu
 
 from logcat.log import z_logger
+import xml.dom.minidom
+
+# UI模板配置文件路径
+menus_ui_config_file_path = "./config/menus_ui.xml"
 
 
 class Controller(object):
+    def __init__(self):
+        self.mainWindow = None
+
     def setupUi(self, parent):
         """
        通过QAction进行顶部菜单及菜单行为初始化
        QAction可以操作菜单栏,工具栏,或自定义键盘快捷键
        :parent: mainWindow
        """
+        self.mainWindow = parent
         menubar = parent.menuBar()
         menubar.setGeometry(QtCore.QRect(0, 0, 705, 23))
         menubar.setObjectName("menubar")
@@ -19,59 +27,39 @@ class Controller(object):
 
         z_logger.debug('initMenuBar :: actions')
 
-        # menu_actions = [act_close, act_exit]
-        menus = [
-            _translate("Menu_Main", "菜单"),
-            _translate("Menu_Edit", "编辑"),
-            _translate("Menu_About", "关于")
-        ]
+        dom = xml.dom.minidom.parse(menus_ui_config_file_path)
+        root = dom.documentElement
+        menu_list = root.getElementsByTagName("menu")
+        for menu in menu_list:
+            _menuGroup: QMenu = menubar.addMenu(menu.getAttribute("name"))
+            action_list = menu.getElementsByTagName("action")
+            for action in action_list:
+                _action: QAction = QAction(parent=parent)
+                actionName = action.getAttribute("name")
+                _action.setText(actionName)
 
-        # 【菜单栏】
-        #   新建连接
-        new_connect = QAction(QIcon('./res/icons/add_new.png'), '&NewConnect', parent)
-        new_connect.setObjectName("new_connect")
-        new_connect.setShortcut('Ctrl+N')  # 自定义快捷键
-        new_connect.setText(_translate("MainWindow", "连接新设备"))
-        new_connect.triggered.connect(parent.show_new_device_dialog)
-        #   关闭
-        act_close = QAction(QIcon('./res/icons/close.png'), '&close', parent)
-        act_close.setObjectName("act_close")
-        act_close.setText(_translate("MainWindow", "Close"))
-        #   退出应用
-        act_exit = QAction(QIcon('./res/icons/app_quit.png'), '&Exit', parent)
-        act_exit.setObjectName("exit_app")
-        act_exit.setShortcut('Ctrl+Q')  # 自定义快捷键
-        act_exit.setStatusTip('Exit application')  # 自定义提示
-        act_exit.triggered.connect(qApp.quit)  # 建立信号连接槽
-        act_exit.setText(_translate("MainWindow", "退出"))
-        # act_close.setText("退出")
+                state = action.getAttribute("state")
+                if state == "disable":
+                    _action.setEnabled(False)
 
-        # 【关于】
-        logAction = QAction(parent)
-        logAction.setObjectName("act_log")
-        logAction.setText(_translate("act_log", "打开日志目录"))
-        logAction.triggered.connect(parent.show_new_device_dialog)
-
-        aboutAction = QAction(QIcon('./res/icons/add_new.png'), '&About', parent)
-        aboutAction.setObjectName("act_about")
-        aboutAction.setText(_translate("act_about", "关于"))
-        aboutAction.triggered.connect(parent.show_new_device_dialog)
-
-        actions = {
-            # 菜单1对应的action
-            menus[0]: [new_connect, act_close, act_exit],
-            # 菜单2对应的action
-            menus[1]: [act_exit],
-            menus[2]: [logAction, aboutAction]
-        }
-
-        z_logger.debug('initMenuBars')
-        # 初始化菜单项
-        for menu in menus:
-            menu_item = menubar.addMenu(menu)
-            acts = actions[menu]
-            for action in acts:
-                menu_item.addAction(action)
-            menubar.addAction(menu_item.menuAction())
-        # 将menu添加到menubar上
+                attrs = action.getElementsByTagName("attr")
+                for attr in attrs:
+                    _key = attr.getAttribute('name')
+                    _value = attr.firstChild.nodeValue
+                    if _key == "shortcut":
+                        _action.setShortcut(_value)
+                    elif _key == "icon":
+                        _action.setIcon(QIcon(_value))
+                    elif _key == "action":
+                        _action.triggered.connect(lambda checked, cmd=_value: self.onActionClicked(cmd))
+                _menuGroup.addAction(_action)
+            menubar.addAction(_menuGroup.menuAction())
         parent.setMenuBar(menubar)
+
+    def onActionClicked(self, cmd):
+        if cmd == "m_connect_new_device":
+            self.mainWindow.show_new_device_dialog()
+        elif cmd == "m_close_app":
+            qApp.quit()
+        else:
+            z_logger.error(f"该命令还未实现:{cmd}")
