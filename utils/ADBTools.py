6894245d2fb756956073f50d160ee6f4c9b528a0
zekeWong <kivensolo@gmail.com>
2023-12-28 16:13:06 +0800

    Title(feat): 增加获取可用进程的名称和pid等信息。 Cause：ADB工具方法增加。 Solution：N/A。 Effect: N/A Reviewer: N/A 附加说明：N/A

--- utils/ADBTools.py
+++ utils/ADBTools.py
@@ -1,4 +1,4 @@
-import string
+import re
 import subprocess
 
 from logcat.log import z_logger
@@ -28,6 +28,35 @@ class ActionCmdParams:
         else:
             _full_cmd = "adb -s {0} {1}".format(self.target_device_ip, _cmd)
         return _full_cmd
+
+
+def get_filter_processes():
+    """
+    获取进程信息
+    ['USER', 'PID', 'PPID', 'VSZ', 'RSS', 'WCHAN', 'ADDR', 'S', 'NAME']
+    :return:
+    """
+    output = subprocess.check_output("adb shell ps", shell=True).decode("utf-8")
+    processes = []
+    for line in output.splitlines()[1:]:
+        columns = line.split()
+        user = columns[0]
+        if not str(user).startswith("u0_") \
+                and user != "system" and user != "bluetooth":
+            # 如果不是用户\蓝牙\系统进程,则过滤掉，比如root、wifi、shell等用户
+            continue
+        # if columns[1] == "PID": # 可能是列表头
+        #     continue
+        pid = int(columns[1])   # 获取pid值
+        if pid <= 1000:
+            # 过滤系统进程
+            continue
+        name = columns[-1]      # 获取最后一列 Name名称
+        if str(name).startswith("["):
+            # 过滤[aml_pwrsave_wq] 这种进程
+            continue
+        processes.append((user, pid, name))
+    return processes
 
 
 class ADBTools:
@@ -110,3 +139,10 @@ class ADBTools:
             z_logger.info(f"Screenshot captured successfully! > {save_path}")
         else:
             z_logger.error("Failed to capture screenshot.")
+
+    def get_running_process(self):
+        filtered_processes = get_filter_processes()
+        # 按照A-Z顺序对进程名称进行排序
+        sorted_processes = sorted(filtered_processes, key=lambda x: x[2])
+        for user, pid, p_name in sorted_processes:
+            print(f"Process Name: {p_name}, PID: {pid}")
