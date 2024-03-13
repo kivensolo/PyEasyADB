1dbba3b97d6c76779368e1a08ead2b10bdbd85fc
zekeWong <kivensolo@gmail.com>
2024-03-13 18:07:49 +0800

    Title(fix):修复真机设备名称为纯数字时，依旧给其加入了端口信息。导致adb devices识别到的名称与其不一致，无法正确显示在线状态。 Solution：将名称判断逻辑取反，没点号的就认为都不是ip地址。 Effect: N/A Reviewer: N/A 附加说明：N/A

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -695,9 +695,9 @@ class MainWindow(BaseWindow):
                 if not exist:
                     z_logger.debug(f"[Refresh Devices] Find new device {device_name}, save to database.")
                     _port = "0"
-                    if any(char.isalpha() for char in device_name):
-                        # 检查是否包含任意字母字符
-                        z_logger.debug("[Refresh Devices] 设备信息包含字符, 不做处理")
+                    if not device_name.contains("."):
+                        # 不包含ip格式的点,则说明是设备名称，可能纯数字，也可能是字符和数据混杂
+                        z_logger.debug("[Refresh Devices] 设备标识为名称，非ip格式, 不做处理")
                     elif ":" not in device_name:
                         _port = "5555"
 
